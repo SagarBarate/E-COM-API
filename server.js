ded3de38d491d625cbab15caa3d6e7ce9c23dd93
Sagar Barate <sagarbarate2907@com.com>
2024-01-03 20:56:24 +0530

    usersdatacreation

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 // 1. Import express
 import express from 'express';
 import productRouter from './src/features/product/product.routes.js';
+import userRouter from './src/features/user/user.routes.js';
 import bodyParser from 'body-parser';
 
 
 // 2. Create Server
 const server = express();
 
-server.use(bodyParser.json());
+server.use(bodyParser.json());// Its is the middleware which helps when user send req in json format then it will make it available in req.body for further processesing is routes
 
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/products
 server.use("/api/products", productRouter);
+server.use("/api/users", userRouter);
 
 // 3. Default request handler
 server.get('/', (req, res)=>{
--- src/features/user/user.model.js
+++ src/features/user/user.model.js
@@ -1,21 +1,27 @@
-export class UserModel{
-    constructor(name, email, password, type){
+export default class UserModel{
+    constructor(name, email, password, type, id){
         this.name= name;
         this.email= email;
         this.password= password;
         this.type= type;
+        this.id= id;
 
     }
 
     static SignUp(name, email, password, type){
-        const newUser = new UserModel(name, emial, password, type);
+
+        const newUser = new UserModel(name, email, password, type);
+        newUser.id= users.length+1;
         users.push(newUser);
+
+        return newUser;
     }
 
     static SignIn(email, password){
         const user= users.find((u)=> u.email ==email && u.password == password);
         return user;
     }
+
 }
 
 var users = [
